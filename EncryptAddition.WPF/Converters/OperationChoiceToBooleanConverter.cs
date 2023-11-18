14db53ee18471eee364fe7d086702824682284a1
Deni Mastori <denimastori02@gmail.com>
2023-11-18 15:24:29 +0200

    [fix] fix default radio button choices for encrypt tab

---
+++ EncryptAddition.WPF/Converters/OperationChoiceToBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using EncryptAddition.WPF.DataTypes;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EncryptAddition.WPF.Converters
+{
+    public class OperationChoiceToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is OperationChoice choice)
+            {
+                if (choice == OperationChoice.ENCRYPTION)
+                    return true;
+                else
+                    return false;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
